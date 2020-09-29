4d50c186afedc86f21f27770cbeab4b879a5d956
BagusSatria123 <satriabagusputra123@gmail.com>
2020-09-29 08:48:48 +0700

    Detail Activity,Main Activity Done

---
+++ app/src/main/java/id/putraprima/mobile06siplecalcsolution/MainActivity.java
@@ -0,0 +1,21 @@
+package id.putraprima.mobile06siplecalcsolution;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
