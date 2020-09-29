8a8b4d9907f14203c6cf428356c12bb3b7dff138
RioCyber936 <adirio205@gmail.com>
2020-09-29 09:23:01 +0700

    Date Picker Sudah Ditambahkan

--- app/src/main/java/id/putraprima/mobile06siplecalcsolution/MainActivity.java
+++ app/src/main/java/id/putraprima/mobile06siplecalcsolution/MainActivity.java
@@ -2,20 +2,49 @@ package id.putraprima.mobile06siplecalcsolution;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.DatePicker;
 import android.widget.Toast;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    DatePickerDialog picker;
+    EditText edit_tanggal_lahir;
+    Button btnGet;
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edit_tanggal_lahir=(EditText) findViewById(R.id.edit_text_tanggal_lahir);
+        edit_tanggal_lahir.setInputType(InputType.TYPE_NULL);
+        edit_tanggal_lahir.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+
+                picker = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                edit_tanggal_lahir.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
     }
 }
